8feea62635020063a7cc5037f3a0bbe490a9714b
Mostafijur Rahman <rahman.mostafijur@acciptra.com>
2026-08-09 23:40:57 +0600

    Add docker and docker compose setup

--- database.py
+++ database.py
@@ -1,9 +1,12 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
+import os
+from dotenv import load_dotenv
 
-database_url = "postgresql://postgres:16189251@localhost:5432/taskmanager"
+load_dotenv()
+DATABASE_URL = os.getenv("DATABASE_URL")
 
-engine = create_engine(database_url)
+engine = create_engine(DATABASE_URL)
 
 session_local = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
