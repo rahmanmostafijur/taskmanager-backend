945fb6b1ce6c26a168ba91c7f824c9160e7afad1
Mostafijur Rahman <rahman.mostafijur@acciptra.com>
2026-08-12 01:34:28 +0600

    Run Alembic migrations on container startup

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ import model
 from database import engine
 from routers import items, tasks, users
 
-model.Base.metadata.create_all(bind=engine)
+# model.Base.metadata.create_all(bind=engine)
 
 app = FastAPI()
 
