004addac4bfd0adf94b7a029c7e0e5d61a54c239
Mostafijur Rahman <rahman.mostafijur@acciptra.com>
2026-08-12 14:28:40 +0600

    initial commit

--- main.py
+++ main.py
@@ -2,10 +2,18 @@ from fastapi import FastAPI
 import model
 from database import engine
 from routers import items, tasks, users
+from fastapi.middleware.cors import CORSMiddleware
 
 # model.Base.metadata.create_all(bind=engine)
 
 app = FastAPI()
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 app.include_router(items.router)
 app.include_router(tasks.router)
